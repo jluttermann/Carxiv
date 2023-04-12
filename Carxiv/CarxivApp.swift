d46e380aa53e4bbe2b111091c631975d29ec31ee
Jan Luttermann <jan@Jans-Air.fritz.box>
2023-04-12 15:07:36 +0200

    Init: initial commit with base app files

---
+++ Carxiv/CarxivApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarxivApp.swift
+//  Carxiv
+//
+//  Created by Jan Luttermann on 12.04.23.
+//
+
+import SwiftUI
+
+@main
+struct CarxivApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
