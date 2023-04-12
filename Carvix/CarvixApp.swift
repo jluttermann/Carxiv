3c3a8b53628588d1b804578ea4ae018280d3e7c8
Jan Luttermann <jan@Jans-Air.fritz.box>
2023-04-12 15:29:47 +0200

    init: reinitial commit

---
+++ Carvix/CarvixApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarvixApp.swift
+//  Carvix
+//
+//  Created by Jan Luttermann on 12.04.23.
+//
+
+import SwiftUI
+
+@main
+struct CarvixApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Carvix/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Carvix
+//
+//  Created by Jan Luttermann on 12.04.23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
